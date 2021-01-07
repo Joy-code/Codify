a6e9094d403d56777718bae3a53b17e4ba529b91
joy-code <joy.t.quan@gmail.com>
2021-01-07 16:30:16 -0500

    db

--- codifySite/codifySite/settings.py
+++ codifySite/codifySite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'codifySite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'codingqdb',
+        'USER': 'joyq',
+        'PASSWORD': 'Nautilus',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
